87ca1404efca322da79639d524737e8427901caf
kunstler <georges.kunstler@gmail.com>
2021-12-09 12:10:29 +0100

    first commit

--- R/ReadChelsa.R
+++ R/ReadChelsa.R
@@ -1,5 +1,12 @@
+read_mask <- function(path = "mask"){
+  require(terra)
+  require(rgdal)
+  mask <- rast(file.path(path, "chelsa-w5e5v1.0_obsclim_mask_30arcsec_global.nc"))
+  return(mask)
+}
 
-read_stack_var_year <- function(var = 'pr', year = 2018, 
+
+read_chelsa_stack_var_year <- function(var = 'pr', year = 2018, 
                                 path = "envicloud/chelsa/chelsa_V2/GLOBAL/monthly"){
   require(terra)
   require(rgdal)
@@ -7,7 +14,7 @@ read_stack_var_year <- function(var = 'pr', year = 2018,
   
   xstring <- paste('_',var, '_', sep='')
   files   <- list.files( file.path(path, var), full.names = T)
-  ms <- c(paste0("0",1:9), 10,12)
+  ms <- c(paste0("0",1:9), 10:12)
   
   files_sel <- paste0("CHELSA_pr_",ms,"_",year, "_V.2.1.tif")
   
@@ -18,108 +25,41 @@ read_stack_var_year <- function(var = 'pr', year = 2018,
     layer_t <- rast(file.path(path, var,files_sel[m]))
     add(layers) <- layer_t
   }
+names(layers) <- gsub("_V.2.1", "", gsub("CHELSA_", "", names(layers)))
 return(layers)
   
 }
 
-extract_var_year <- function(coords= NA, var = "pr", year = 2018){
-  # df <- read.csv("FunDiv_plots_Nadja.csv", stringsAsFactors=FALSE)
-  df <- read.csv("mastifPlotData.csv", stringsAsFactors=FALSE)
-  #coords <- df[, c("longitude", "latitude")] 
-  #names(coords) <-c("lon", "lat")
+extract_chelsa_var_year <- function(coords, mask, var = "pr", year = 2018){
   coords <- df[, c("lon", "lat")] 
   
-  stacks <- read_stack_var_year(var, year)
+  stacks <- read_chelsa_stack_var_year(var, year)
+  stacks <- mask(stacks, mask)
   res <- terra::extract(stacks, coords)
   return(res)
 }
 
-
-library(tictoc)
-tic()
-res <- extract_var_year()
-toc()
-sum(is.na(res[, 1]))
-
-
-tileChelsa2 <- function(vars = c('pr'), 
-                       path = "/Users/jimclark/makeMastOnJimClark/makeMast/climateBuild/terraClimateFiles"){
-  vars = c('pr')
-  path <- "envicloud/chelsa/chelsa_V2/GLOBAL/monthly/pr"
-  # generates tiles for chelsa data, run when new years are added
+extract_chelsa_var_years <- function(coords, mask, 
+                                     var = "pr", 
+                                     years= 1983:2018){
   
-  require(raster)
-  require(rgdal)
-  
-  # wpath <- "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/makeMastOnJimClark/makeMast/climateBuild/chelsaTiles"
-  wpath <- "envicloud/chelsa/chelsa_V2/GLOBAL/monthly"
-  
-  
-  monNames <- as.character(1:12)
-  monNames[ nchar(monNames) == 1] <- paste('0', monNames[ nchar(monNames) == 1], sep='' )
-  
-  mstart <- 1
-  ystart <- 1
-  
-  for(k in 1:length(vars)){
-    
-    xstring <- paste('_',vars[k], '_', sep='')
-    files   <- list.files( path, full.names = T)
-    files   <- files[ grep( xstring, files ) ]
-    tmp     <- columnSplit(files,  xstring)
-    dates   <- columnSplit( tmp[,2], '_' )
-    months  <- dates[,1]
-    years   <- dates[,2]
-    
-    if(length(years) == 0)next
-    
-    kpath <- paste(wpath, vars[k], sep='/')
-    
-    for(j in ystart:length(years)){
-      
-      library(envirem)
-      library(rgdal)
-      jfile <- files[ grep( years[j], files ) ]
-      mj    <- length( jfile )
-      
-      tiles <- numeric(0)
-      xk    <- numeric(0)
-      
-      for(m in mstart:mj){
-        layer <- raster( jfile[m] )
-        
-        dname <- paste( vars[k], years[j], monNames[m], sep='/' )
-        if( !dir.exists(vars[k]) ) dir.create(vars[k])
-        if( !dir.exists(paste( vars[k], years[j], sep= "/")) ) dir.create(paste( vars[k], years[j], sep= "/"))
-        if( !dir.exists(paste( vars[k], years[j], monNames[m], sep= "/")) ) dir.create(paste( vars[k], years[j], monNames[m], sep= "/"))
-        
-        
-        n_vertical_tile = 25   
-        split_raster(jfile[m], s = n_vertical_tile, outputDir = dname,
-                     gdalinfoPath = "/Library/Frameworks/GDAL.framework/Programs/gdalinfo" , 
-                     gdal_translatePath = "/Library/Frameworks/GDAL.framework/Programs/gdal_translate")
-   rm(layer)
-   gc()
-      }
-    }
+ list_var_years <- vector("list")
+  for (y in seq_len(length(years))){
+    list_var_years[y] <- extract_chelsa_var_year(coords, mask, var, years[y])
   }
+ names(list_var_years) <- paste0(var, "_", years)
+ res <- as.matrix(bind_cols(list_var_years))
+ return(res)
 }
 
+# library(tictoc)
+# tic()
+# res <- extract_var_year()
+# toc()
+# sum(is.na(res[, 1]))
 
 
 
 
 
-
-
-
-start <- Sys.time()
-tileChelsa()
-end  <- Sys.time()
-t1 <- end - start
-
-start <- Sys.time()
-tileChelsa2()
-end  <- Sys.time()
-t2 <- end - start
 
---
+++ _targets.R
@@ -0,0 +1,41 @@
+# _targets.R file
+library(targets)
+lapply(grep("R$", list.files("R"), value = TRUE), function(x) source(file.path("R", x)))
+
+options(tidyverse.quiet = TRUE)
+tar_option_set(packages = c("terra", "rgdal", "dplyr", "ggplot2"))
+list(
+  tar_target(coords,"NFI/FUNDIV_coordinates.csv",
+             format = "file"
+  ),
+  tar_target(
+    pr_2018,
+    extract_chelsa_var_year(coords, mask)
+  ),
+  tar_target(
+    pr_all,
+    extract_chelsa_var_years(coords, mask)
+  ),
+  tar_target(
+    mask,
+    read_mask
+  )  ,
+  tar_target(
+    pr_chelsa_1983_2018,
+    extract_chelsa_var_years(coords, mask, var = "pr")
+  )  ,
+  tar_target(
+    tas_chelsa_1983_2018,
+    extract_chelsa_var_years(coords, mask, var = "tas")
+  )  ,
+  tar_target(
+    tasmin_chelsa_1983_2018,
+    extract_chelsa_var_years(coords, mask, var = "tasmin")
+  )  ,
+  tar_target(
+    tasmax_chelsa_1983_2018,
+    extract_chelsa_var_years(coords, mask, var = "tasmax")
+  )  
+  
+)
+
